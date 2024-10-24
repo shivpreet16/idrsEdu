7a65b4d05ced6656c2bcff6fba2823b2bd2e3d89
Shivpreet <shivpreet16@gmail.com>
2024-10-24 19:49:23 +0530

    added chairperson on landing page

--- src/components/KnowUs.jsx
+++ src/components/KnowUs.jsx
@@ -8,18 +8,28 @@ const KnowUs = () => {
     const principal={
         img:'/atul_hans.jpg',
         image_type:'round',
-        text:`"At IDRS, we are dedicated to shaping curious minds and building a strong foundation for our students' future. Our aim is to guide each child to discover their strengths, embrace challenges, and emerge as confident, capable individuals ready to lead with purpose." — Atul Hans, Principal, IDRS School`
+        text:`"At IDRS, we are dedicated to shaping curious minds and building a strong foundation for our students' future. Our aim is to guide each child to discover their strengths, embrace challenges, and emerge as confident, capable individuals ready to lead with purpose."`,
+        name:'Atul Hans, Principal, IDRS School'
     }
     const vicePrincipal={
         img:'/ruchi_manhas.jpg',
         image_type:'round',
-        text:`"At IDRS, we foster a nurturing environment where students are encouraged to explore, excel, and develop into confident leaders of tomorrow." — Ruchi Manhas, Vice Principal, IDRS School`
+        text:`"At IDRS, we foster a nurturing environment where students are encouraged to explore, excel, and develop into confident leaders of tomorrow."`,
+        name:'Ruchi Manhas, Vice Principal, IDRS School'
+      }
+      const chairPerson={
+        img:'/supinder_kour.jpg',
+        image_type:'round',
+        text:`"Under her guidance, our school has flourished, creating an environment where every child is encouraged to discover their potential and pursue their passions. Supinder ma’am’s commitment to fostering a nurturing and innovative atmosphere reflects her belief in the transformative power of education."`,
+        name:'Supinder Kour , Chairperson, IDRS School'
+        
     }
   return (
     <div className="h-fit w-screen bg-primary">
       <KnowUsCard img={schoolInfo.img} text={schoolInfo.text} right={true}/>
-      <KnowUsCard img={principal.img} text={principal.text} right={false} image_type={principal.image_type}/>
-      <KnowUsCard img={vicePrincipal.img} text={vicePrincipal.text} right={true} image_type={vicePrincipal.image_type}/>
+      <KnowUsCard img={chairPerson.img} text={chairPerson.text} right={true} image_type={chairPerson.image_type} name={chairPerson.name}/>
+      <KnowUsCard img={principal.img} text={principal.text} right={false} image_type={principal.image_type} name={principal.name}/>
+      <KnowUsCard img={vicePrincipal.img} text={vicePrincipal.text} right={true} image_type={vicePrincipal.image_type} name={vicePrincipal.name}/>
     </div>
   );
 };
--- src/components/KnowUsCard.jsx
+++ src/components/KnowUsCard.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 
-const KnowUsCard = ({ img, text, right, image_type }) => {
+const KnowUsCard = ({ img, text, right, image_type,name }) => {
     
   return (
     <div className={`h-1/2 flex ${right ? "flex-row" : "flex-row-reverse"}`}>
-      <div className={`w-1/2 flex justify-center items-center ${!right?'bg-secondary':'bg-primary'}`}>
+      <div className={`w-1/2 flex justify-center flex-col gap-2 items-center ${!right?'bg-secondary':'bg-primary'}`}>
         <img
           src={img}
           alt=""
           className={`${image_type === 'round' ? 'rounded-full h-[300px] w-[300px] object-cover' : 'h-full w-full object-cover'}`}
         />
+        <span className={`${right?'text-secondary':'text-primary'} italic`}>{name}</span>
       </div>
       <div
         className={`${
